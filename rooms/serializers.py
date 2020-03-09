f730b707da0f5af4614da76d91e0753b0a226765
parkyes90 <parkyes90@gmail.com>
2020-03-09 23:16:13 +0900

    feat($ProjectFilePath): Add create room

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -4,10 +4,31 @@ from rooms.models import Room
 from users.serializers import UserSerializer
 
 
-class RoomSerializer(serializers.ModelSerializer):
+class ReadRoomSerializer(serializers.ModelSerializer):
 
     user = UserSerializer()
 
     class Meta:
         model = Room
         exclude = ("modified",)
+
+
+class WriteRoomSerializer(serializers.Serializer):
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        print(validated_data)
+        return Room.objects.create(**validated_data)
+
+    name = serializers.CharField(max_length=140)
+    address = serializers.CharField(max_length=140)
+    price = serializers.IntegerField()
+    beds = serializers.IntegerField(default=1)
+    lat = serializers.DecimalField(max_digits=10, decimal_places=6)
+    lng = serializers.DecimalField(max_digits=10, decimal_places=6)
+    bedrooms = serializers.IntegerField(default=1)
+    bathrooms = serializers.IntegerField(default=1)
+    check_in = serializers.TimeField(default="00:00:00")
+    check_out = serializers.TimeField(default="00:00:00")
+    instant_book = serializers.BooleanField(default=False)
