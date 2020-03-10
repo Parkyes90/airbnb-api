c1920c8c1cd1258dc14df70191131a4330ae6cc2
parkyes90 <parkyes90@gmail.com>
2020-03-10 18:02:08 +0900

    feat($ProjectFilePath): Add create user

--- users/serializers.py
+++ users/serializers.py
@@ -16,23 +16,24 @@ class RelatedUserSerializer(serializers.ModelSerializer):
         )
 
 
-class ReadUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        exclude = (
-            "groups",
-            "user_permissions",
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost",
             "password",
-            "last_login",
-            "is_superuser",
-            "is_staff",
-            "is_active",
-            "date_joined",
-            "favs",
         )
+        read_only_fields = ("id", "superhost", "avatar")
 
-
-class WriteUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("username", "first_name", "last_name", "email")
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- users/views.py
+++ users/views.py
@@ -6,8 +6,19 @@ from rest_framework.views import APIView
 
 from rooms.models import Room
 from rooms.serializers import RoomSerializer
-from .serializers import ReadUserSerializer, WriteUserSerializer
+from .serializers import UserSerializer
 from .models import User
+
+
+class UsersView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer.save()
+        return Response(serializer.data)
 
 
 class MeView(APIView):
@@ -15,10 +26,10 @@ class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response(ReadUserSerializer(request.user).data)
+        return Response(UserSerializer(request.user).data)
 
     def put(self, request):
-        serializer = WriteUserSerializer(
+        serializer = UserSerializer(
             request.user, data=request.data, partial=True
         )
         if not serializer.is_valid():
@@ -31,7 +42,7 @@ class MeView(APIView):
 def user_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
-        return Response(ReadUserSerializer(user).data)
+        return Response(UserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
